c035e0d2681b014097614f3345ff90e0b29ef971
Özcan Fatih Can <ozcanfatihcann@gmail.com>
2023-11-13 19:23:48 +0300

    food page update

--- lib/view/Food/page/foods_page.dart
+++ lib/view/Food/page/foods_page.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class FoodsPageView extends StatelessWidget {
   const FoodsPageView({super.key});
-  final double spaceObjects = 25;
-  final String textHint = "Yemek ara";
+  final double spaceObjects = 20;
+  final String title = 'Yemek';
+  final String title2 = 'Turşu';
+  final String title3 = 'Reçel';
+  final String title4 = 'İçecek';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,18 +17,25 @@ class FoodsPageView extends StatelessWidget {
           children: [
             const FoodPageHeadText(),
             SizedBox(height: spaceObjects),
-            Padding(
+            const Padding(
               padding: _FoodPagePaddings.pagePaddingx,
-              child: TextField(
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.search_rounded),
-                  hintText: textHint,
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(30),
-                    ),
-                  ),
-                ),
+              child: FoodPageSearchBarWidget(),
+            ),
+            SizedBox(height: spaceObjects),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FoodPageButton(title: title, onPressed: () {}),
+                  FoodPageButton(title: title2, onPressed: () {}),
+                  FoodPageButton(title: title3, onPressed: () {}),
+                  FoodPageButton(title: title4, onPressed: () {}),
+                  FoodPageButton(title: title2, onPressed: () {}),
+                  FoodPageButton(title: title3, onPressed: () {}),
+                  FoodPageButton(title: title4, onPressed: () {}),
+                  FoodPageButton(title: title, onPressed: () {}),
+                ],
               ),
             )
           ],
@@ -33,6 +43,28 @@ class FoodsPageView extends StatelessWidget {
       ),
       drawer: const Drawer(
         backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
+
+class FoodPageSearchBarWidget extends StatelessWidget {
+  const FoodPageSearchBarWidget({
+    super.key,
+  });
+  final String textHint = "Yemek ara";
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.search_rounded),
+        hintText: textHint,
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
       ),
     );
   }
@@ -48,20 +80,51 @@ class FoodPageHeadText extends StatelessWidget
     return Text(
       headText,
       style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-          color: _FoodPageColors.headColor, fontWeight: _FoodPageFont.headFont),
+            color: _FoodPageColors.headColor,
+            fontWeight: _FoodPageFont.headFont,
+          ),
     );
   }
 }
 
 mixin _FoodPageColors {
   static const Color headColor = Colors.black;
+  static const Color buttonColor = Colors.red;
+  static const Color buttonForeColor = Colors.white;
 }
 
-class _FoodPagePaddings {
-  static const pagePaddingx = EdgeInsets.all(8.0);
-  static const pagePadding2x = EdgeInsets.all(16.0);
+mixin _FoodPagePaddings {
+  static const EdgeInsets pagePaddingx = EdgeInsets.all(8.0);
+  static const EdgeInsets pagePadding2x = EdgeInsets.all(16.0);
 }
 
 mixin _FoodPageFont {
   static const headFont = FontWeight.w500;
+}
+
+class FoodPageButton extends StatelessWidget
+    with _FoodPageColors, _FoodPagePaddings {
+  FoodPageButton({Key? key, required this.title, required this.onPressed})
+      : super(key: key);
+  final String title;
+  final void Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const StadiumBorder(),
+        backgroundColor: _FoodPageColors.buttonColor,
+      ),
+      onPressed: onPressed,
+      child: Padding(
+        padding: _FoodPagePaddings.pagePaddingx,
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.titleSmall?.copyWith(
+              color: _FoodPageColors.buttonForeColor,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
 }
